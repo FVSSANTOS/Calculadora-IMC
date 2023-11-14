ffb91b4e14800bf1c826b0ae2d02df421d2c4445
Flávio Vinícius <84485936+FVSSANTOS@users.noreply.github.com>
2023-11-13 21:45:38 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+const form = document.querySelector("#form");
+
+form.addEventListener('submit', function (event){
+    event.preventDefault();
+    const inputPeso = event.target.querySelector('#peso');
+    const inputAltura = event.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if(!peso || !altura){
+        setResultado('Peso ou altura inválido', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+    const msg = `Seu IMC é ${imc} (${nivelImc})`; 
+    setResultado(msg, true);
+});
+
+function getImc(peso,altura){
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function getNivelImc(img){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1','Obesidade grau 2','Obesidade grau 3'];
+
+    if(imc >= 39.9){
+        return nivel[5];
+    }
+
+    if(imc >= 34.9){
+        return nivel[4];
+    }
+
+    if(imc >= 29.9){
+        return nivel[3];
+    }
+
+    if(imc >= 24.9){
+        return nivel[2];
+    }
+
+    if(imc >= 18.5){
+        return nivel[1];
+    }
+
+    if(imc < 18.5){
+        return nivel[0];
+    }
+}
+
+function criaP(){
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(mensagem, isValid){
+    const resultado = documen.querySelector("#resultado");
+    resultado.innerHtml = '';
+    const p = criaP();
+    
+    if(isValid){
+        p.classList.add('paragrafo-resultado');
+    }else{
+        p.classList.add('bad');
+    }
+    p.innerHTML = mensagem;
+    resultado.appendChild(p);
+
+}
